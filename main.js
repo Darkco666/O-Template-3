fe2597f785b96a90fd64b0c81187da9c59ce3280
M.N.S <whatifm666@gmail.com>
2024-02-20 09:06:46 +0200

    Some changes on fetures

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+let upButton = document.querySelector(".up-arrow");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 1000) {
+    upButton.style.display = "block";
+  } else {
+    upButton.style.display = "none";
+  }
+});
+
+upButton.onclick = () =>
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+
 let data = new XMLHttpRequest();
 data.open("Get", "data.json");
 data.send();
@@ -5,7 +21,8 @@ data.send();
 let userName = document.querySelector(".logo");
 let h1Text = document.querySelector(".landing .text h1");
 
-data.onreadystatechange = function () {
+data.onload = function () {
+  // Or Using onreadstatechange
   if (this.readyState === 4 && this.status === 200) {
     let dataJs = JSON.parse(this.responseText);
     if (dataJs[0].Name) {
@@ -24,41 +41,36 @@ data.onreadystatechange = function () {
   }
 };
 
-let upButton = document.querySelector(".up-arrow");
-
-window.onscroll = function () {
-  if (window.scrollY >= 1000) {
-    upButton.style.display = "block";
-  } else {
-    upButton.style.display = "none";
-  }
-};
-
-upButton.addEventListener("click", () =>
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  })
-);
-
 let section = document.querySelector(".our-skills");
 let progressSpans = document.querySelectorAll(
   ".our-skills .skills .skill .the-progress span "
 );
-// let spansNum = document.querySelectorAll(".our-skills .skills .skill h3 span");
+let spansNum = document.querySelectorAll(".our-skills .skills .skill h3 span");
 
 window.onscroll = function () {
   if (window.scrollY >= section.offsetTop - 250) {
     progressSpans.forEach((span) => {
-      for (
-        let i = 0;
-        i <= parseInt(span.getAttribute("full-width"));
-        i += 0.1
-      ) {
-        if (span.style.width != `${span.getAttribute("full-width")}%`) {
-          span.style.width = `${i}%`;
-        }
-      }
+      let width = 0,
+        fullWidth = setInterval(() => {
+          if (span.style.width != `${span.getAttribute("full-width")}%`) {
+            span.style.width = `${width}%`;
+            width++;
+          } else {
+            clearInterval(fullWidth);
+          }
+        }, 0);
+    });
+
+    spansNum.forEach((span) => {
+      let width = 0,
+        fullWidth = setInterval(() => {
+          if (span.textContent != `${span.getAttribute("full-width")}%`) {
+            span.textContent = `${width}%`;
+            width++;
+          } else {
+            clearInterval(fullWidth);
+          }
+        }, 10);
     });
   }
 };
